43447703c57eac3c4d25863b14ea367c221add88
rajeshgopal <rajeshsmyl@gmail.com>
2018-01-15 16:24:29 +0530

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -15,6 +15,12 @@ resource "aws_security_group" "allow-ssh" {
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   } 
+  ingress {
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  } 
 tags {
     Name = "allow-ssh"
   }
